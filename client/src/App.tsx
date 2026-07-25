b28b9e81cba85b5705b206c4452ec1b67ffa2808
AriSetiaHinanda <ari.hinanda23@student.uisi.ac.id>
2026-07-25 18:01:52 +0700

    feat: tambah halaman frontend KreatorPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LandingPage } from "./pages/LandingPage";
+import { KreatorPage } from "./pages/KreatorPage";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { CreatorDashboard } from "./pages/dashboard/creator/Dashboard";
@@ -7,6 +9,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/kreator" element={<KreatorPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard/creator" element={<CreatorDashboard />} />
